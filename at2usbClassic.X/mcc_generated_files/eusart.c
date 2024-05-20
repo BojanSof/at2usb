b1a51e8a88b3b2c4a400d80ecd91762515b2fb9e
Bojan Sofronievski <bojan.drago@gmail.com>
2024-05-21 00:02:40 +0200

    Add MCLR pull-up resistor in schematic, remove UART RX

--- at2usbClassic.X/mcc_generated_files/eusart.c
+++ at2usbClassic.X/mcc_generated_files/eusart.c
@@ -97,8 +97,8 @@ void EUSART_Initialize(void)
     // SCKP Non-Inverted; BRG16 16bit_generator; WUE disabled; ABDEN disabled; 
     BAUDCON = 0x08;
 
-    // SPEN enabled; RX9 8-bit; CREN enabled; ADDEN disabled; SREN disabled; 
-    RCSTA = 0x90;
+    // SPEN enabled; RX9 8-bit; CREN disabled; ADDEN disabled; SREN disabled; 
+    RCSTA = 0x80;
 
     // TX9 8-bit; TX9D 0; SENDB sync_break_complete; TXEN enabled; SYNC asynchronous; BRGH hi_speed; CSRC slave; 
     TXSTA = 0x24;
--- at2usbClassic.X/mcc_generated_files/pin_manager.h
+++ at2usbClassic.X/mcc_generated_files/pin_manager.h
@@ -96,18 +96,6 @@
 #define KBD_DATA_SetPullup()          do { WPUAbits.WPUA5 = 1; } while(0)
 #define KBD_DATA_ResetPullup()        do { WPUAbits.WPUA5 = 0; } while(0)
 
-// get/set RB5 procedures
-#define RB5_SetHigh()            do { LATBbits.LATB5 = 1; } while(0)
-#define RB5_SetLow()             do { LATBbits.LATB5 = 0; } while(0)
-#define RB5_Toggle()             do { LATBbits.LATB5 = ~LATBbits.LATB5; } while(0)
-#define RB5_GetValue()              PORTBbits.RB5
-#define RB5_SetDigitalInput()    do { TRISBbits.TRISB5 = 1; } while(0)
-#define RB5_SetDigitalOutput()   do { TRISBbits.TRISB5 = 0; } while(0)
-#define RB5_SetPullup()             do { WPUBbits.WPUB5 = 1; } while(0)
-#define RB5_ResetPullup()           do { WPUBbits.WPUB5 = 0; } while(0)
-#define RB5_SetAnalogMode()         do { ANSELBbits.ANSB5 = 1; } while(0)
-#define RB5_SetDigitalMode()        do { ANSELBbits.ANSB5 = 0; } while(0)
-
 // get/set RB7 procedures
 #define RB7_SetHigh()            do { LATBbits.LATB7 = 1; } while(0)
 #define RB7_SetLow()             do { LATBbits.LATB7 = 0; } while(0)
